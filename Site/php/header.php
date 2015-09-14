30739601a759179c77ca3f591fed589b267aa8e1
Alexandre Hamel <hamelbass@outlook.com>
2015-09-14 10:43:08 -0400

    dd

--- Site/php/header.php
+++ Site/php/header.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -9,15 +11,71 @@
 </head>
 
 <body>
-<div class="container">
-<ul class="nav nav-pills">
-    <li role="presentation" class="active"><a href="index.php">Accueil</a></li>
-    <li role="presentation"><a href="#">Profile</a></li>
-    <li role="presentation"><a href="#">Messages</a></li>
-</ul>
 
-    <a href="create_user.php"> <h4>Profile</h4></a>
-    <a href="Login.php"> <h4>Se connecter</h4></a>
+    <div class="navbar-wrapper">
+        <div class="container-fluid">
+            <nav class="navbar navbar-fixed-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="index.php">Accueil</a>
 
+                    </div>
+                    <div id="navbar" class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#" class="">Home</a></li>
 
+                            <li class=" dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Departments <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">View Departments</a></li>
+                                    <li><a href="#">Add New</a></li>
+                                </ul>
+                            </li>
+                            <li class=" dropdown"><a href="#" class="dropdown-toggle " data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Managers <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">View Managers</a></li>
+                                    <li><a href="#">Add New</a></li>
+                                </ul>
+                            </li>
+                            <li class=" dropdown"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Staff <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">View Staff</a></li>
+                                    <li><a href="#">Add New</a></li>
+                                    <li><a href="#">Bulk Upload</a></li>
+                                </ul>
+                            </li>
+                            <li class=" down"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">HR <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Change Time Entry</a></li>
+                                    <li><a href="#">Report</a></li>
+                                </ul>
+                            </li>
 
+                        </ul>
+                        <ul class="nav navbar-nav pull-right">
+                            <li class=" dropdown"><a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                    <?php if(isset($_SESSION['user'])){
+                                        echo $_SESSION["user"];
+                                    }
+                                    else echo "Visiteur";
+                                    ?>
+                                    <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Change Password</a></li>
+                                    <li><a href="#">My Profile</a></li>
+                                </ul>
+                            </li>
+                            <li class=""><a href="#">Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+    <div class="container">
